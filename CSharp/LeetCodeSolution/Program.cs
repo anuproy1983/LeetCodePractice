65f67b3d9f5fddba8d4c47d3a35c9e1c8c7e8717
Anup-PC <Anup-PC@DESKTOP-4FSA8O2>
2025-11-19 22:14:36 -0600

    TwoSum LeetCode problem

---
+++ CSharp/LeetCodeSolution/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var twoSum = new TwoSum();
+            //var result = twoSum.AddSum(new int[] { 2, 7, 11, 15 }, 9);
+            //var result = twoSum.AddSum(new int[] { 3, 2, 4 }, 6);
+            var result = twoSum.AddSum(new int[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 }, 11);
+            Console.WriteLine($"[{result[0]},{result[1]}]");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/LeetCodeSolution/TwoSum.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace LeetCodeSolution
+{
+    // https://leetcode.com/problems/two-sum/
+    public class TwoSum
+    {
+        public int[] AddSum(int[] arrayNum, int target)
+        {
+
+            var dictSum = new Dictionary<int, int>();
+
+            for (int i = 0; i < arrayNum.Length; i++)
+            {
+                var diff = target - arrayNum[i];
+
+                if (dictSum.TryGetValue(diff, out int foundValue))
+                    return new int[] { foundValue, i };
+
+                if (!dictSum.ContainsKey(arrayNum[i]))
+                    dictSum.Add(arrayNum[i], i);
+                    
+            }
+            return null;
+
+
+
+            //for (int i = 0; i < arrayNum.Length; i++)
+            //{
+            //    var diff = target - arrayNum[i];
+
+            //    if (dictSum.TryGetValue(diff, out int foundValue))
+            //        return new int[] { foundValue, i };
+
+            //    if (!dictSum.ContainsKey(arrayNum[i]))
+            //        dictSum.Add(arrayNum[i], i);
+
+            //}
+            //return null;
+
+        }
+    }
+}
