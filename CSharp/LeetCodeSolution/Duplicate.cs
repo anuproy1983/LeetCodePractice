bb23a1f4385dc2764e966f4ac3b2e7b81849d9bc
Anup-PC <Anup-PC@DESKTOP-4FSA8O2>
2026-01-18 11:38:42 -0600

    LeetCode Sum and Duplicate number

---
+++ CSharp/LeetCodeSolution/Duplicate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolution
+{
+    internal class Duplicate
+    {
+        public bool IsDuplicateNumber(int[] nums)
+        {
+            var numSet = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (numSet.Contains(nums[i]))
+                {
+                    return true;
+                }
+                else
+                    numSet.Add(nums[i]);
+            }
+            return false;
+        }
+    }
+}
--- CSharp/LeetCodeSolution/Program.cs
+++ CSharp/LeetCodeSolution/Program.cs
@@ -10,11 +10,24 @@ namespace LeetCodeSolution
     {
         static void Main(string[] args)
         {
+            #region Find Is any number is Duplicate
+            Duplicate duplicate = new Duplicate();
+            Console.WriteLine(duplicate.IsDuplicateNumber(new int[] { 1, 2, 4, 5, 6, 7 }));
+            #endregion
+
+            #region Merge String Alternate
+            var mergeString = new MergeStringAlternate();
+            var mergedResult = mergeString.MergeAlternate("abc", "pqr");
+            Console.WriteLine(mergedResult);
+            #endregion
+
+            #region Add Sum
             var twoSum = new TwoSum();
             //var result = twoSum.AddSum(new int[] { 2, 7, 11, 15 }, 9);
             //var result = twoSum.AddSum(new int[] { 3, 2, 4 }, 6);
             var result = twoSum.AddSum(new int[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 }, 11);
             Console.WriteLine($"[{result[0]},{result[1]}]");
+            #endregion
 
             Console.ReadLine();
         }
